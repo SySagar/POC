d738f93a5328eaf9b2be4c0e1a949c0b6e7610ba
Sagar <sysagar07@gmail.com>
2024-03-29 21:33:40 +0530

    feat: stylex css designing

--- src/lib/Select/options/SelectOption.tsx
+++ src/lib/Select/options/SelectOption.tsx
@@ -1,16 +1,32 @@
 import { FC, ReactNode } from 'react';
+import * as stylex from "@stylexjs/stylex";
 
 type SelectOptionProps = {
   option: ReactNode;
   onClick: () => void;
   selected: boolean;
 };
+
+const styles = stylex.create({
+  option: {
+    padding: '8px 12px',
+    cursor: 'pointer',
+    backgroundColor: {
+      default: '#fff',
+      ":hover": '#D3D3D3',
+    },
+  },
+  selected: {
+    backgroundColor: '#f9f9f9',
+  },
+});
+
 const SelectOption: FC<SelectOptionProps> = ({ option, onClick, selected }) => {
   
   return(
   <div
     role="option"
-    className={`option ${selected ? 'selected' : ''}`}
+    {...stylex.props(styles.option, selected && styles.selected)}
     onClick={onClick}
   >
     {option}
--- src/lib/Select/options/SelectOptionsList.tsx
+++ src/lib/Select/options/SelectOptionsList.tsx
@@ -1,17 +1,38 @@
 import { FC, ReactNode } from 'react';
 import { useSelectContext } from '../hooks/SelectContext';
 import SelectOption from './SelectOption';
+import * as stylex from "@stylexjs/stylex";
 
 type SelectOptionsListProps = {
   options: ReactNode[];
   selectedValue: ReactNode;
 };
+
+const styles = stylex.create({
+  optionsList: {
+    position: 'absolute',
+    top: '100%',
+    left: 0,
+    right: 0,
+    backgroundColor: '#fff',
+    border: '1px solid #ccc',
+    marginTop: '4px',
+    borderRadius: '4px',
+    display: 'none',
+  },
+  open: {
+    display: 'block',
+  },
+});
+
 const SelectOptionsList: FC<SelectOptionsListProps> = ({ options,  selectedValue }) =>{
   const {handleOptionClick,isOpen} = useSelectContext();
 
   console.log('isOpen:', isOpen);
   return (
-  <div className="options-list">
+  <div 
+   {...stylex.props(styles.optionsList, isOpen && styles.open)}
+  >
     {options.map((option, index) => (
       <SelectOption
         key={index}
